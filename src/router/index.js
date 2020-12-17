0c22f3aca2502f2a3d362391a6e82d94945f32fc
Daazzer <Daazzer@github.com>
2020-12-17 13:39:47 +0800

    拆分 router 文件夹

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from '';
+import CityList from './pages/CityList';
+
+const routes = [
+  {
+    path: '/citylist',
+    children:
+  },
+];
