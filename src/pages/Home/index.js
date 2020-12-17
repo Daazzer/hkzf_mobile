0ba6dd6061c1f957d84339248414c7ef9c3a7745
Daazzer <Daazzer@github.com>
2020-12-17 22:51:46 +0800

    引入轮播图组件

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
-function Home() {
-  return (
-    <div>
-      首页
-    </div>
-  );
+import { Component } from 'react';
+import { Carousel } from 'antd-mobile';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+      imgHeight: 176,
+    }
+  }
+  render() {
+    return (
+      <Carousel
+        autoplay
+        infinite
+        autoplayInterval={5000}
+      >
+        {this.state.data.map(val => (
+          <img
+            src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+            alt="a"
+            style={{ width: '100%', verticalAlign: 'top' }}
+            onLoad={() => {
+              // fire window resize event to change height
+              window.dispatchEvent(new Event('resize'));
+              this.setState({ imgHeight: 'auto' });
+            }}
+            key={val}
+          />
+        ))}
+      </Carousel>
+    );
+  }
 }
 
 export default Home;
