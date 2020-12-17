48a62883688c1ff3d974eb9dee277a2a0642b20a
Daazzer <Daazzer@github.com>
2020-12-17 11:29:34 +0800

    antd-mobile 框架按需引入配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Button } from 'antd-mobile';
+
 function App() {
   return (
     <div className="app">
-      hkzf_mobile
+      <Button type="primary">primary</Button>
     </div>
   );
 }
