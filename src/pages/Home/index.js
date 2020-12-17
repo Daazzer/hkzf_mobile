2ff88e83cb9bb5dbadbd720d5b8829d1626e1311
Daazzer <Daazzer@github.com>
2020-12-17 23:45:45 +0800

    首页搜索栏添加与布局

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import { Carousel } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
+import './index.scss';
 
 class Home extends Component {
   constructor() {
@@ -11,25 +12,43 @@ class Home extends Component {
   }
   render() {
     return (
-      <Carousel
-        autoplay
-        infinite
-        autoplayInterval={5000}
-      >
-        {this.state.data.map(val => (
-          <img
-            src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-            alt="a"
-            style={{ width: '100%', verticalAlign: 'top' }}
-            onLoad={() => {
-              // fire window resize event to change height
-              window.dispatchEvent(new Event('resize'));
-              this.setState({ imgHeight: 'auto' });
-            }}
-            key={val}
-          />
-        ))}
-      </Carousel>
+      <div className="home">
+        <Flex className="search-bar">
+          <Flex.Item className="search-bar__search">
+            <Flex>
+              <div className="city">
+                <span>广州</span>
+                <i className="iconfont icon-arrow"></i>
+              </div>
+              <div className="address">
+                <i className="iconfont icon-seach"></i>
+                <span>请输入小区或地址</span>
+              </div>
+            </Flex>
+          </Flex.Item>
+          <i className="search-bar__map iconfont icon-map"></i>
+        </Flex>
+
+        <Carousel
+          autoplay
+          infinite
+          autoplayInterval={5000}
+        >
+          {this.state.data.map(val => (
+            <img
+              src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+              alt="a"
+              style={{ width: '100%', verticalAlign: 'top' }}
+              onLoad={() => {
+                // fire window resize event to change height
+                window.dispatchEvent(new Event('resize'));
+                this.setState({ imgHeight: 'auto' });
+              }}
+              key={val}
+            />
+          ))}
+        </Carousel>
+      </div>
     );
   }
 }
