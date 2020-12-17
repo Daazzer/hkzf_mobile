62fdde2bdf6aee65fc564dc43b6929415884bc50
Daazzer <Daazzer@github.com>
2020-12-17 15:00:18 +0800

    抽离路由并且配置 lazy 动态路由

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import {
-  BrowserRouter as Router,
-  Link,
-  Switch,
-  Route
-} from 'react-router-dom';
-import Home from './pages/Home';
-import CityList from './pages/CityList';
+import Router from './router'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">首页</Link>
-          </li>
-          <li>
-            <Link to="/citylist">城市列表</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/citylist">
-            <CityList />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Router />
     </div>
   );
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,58 @@
+import { lazy, Suspense } from 'react';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Link,
   Switch,
   Route
 } from 'react-router-dom';
-import Home from '';
-import CityList from './pages/CityList';
 
 const routes = [
   {
     path: '/citylist',
-    children:
+    title: '城市列表',
+    component: lazy(() => import('@/pages/CityList'))
+  },
+  {
+    path: '/',
+    title: '首页',
+    component: lazy(() => import('@/pages/Home')),
+    exact: true
   },
 ];
+
+function RouterLink() {
+  return (
+    <ul>
+      {routes.map(route =>
+        <li key={route.title}>
+          <Link to={route.path}>{route.title}</Link>
+        </li>
+      )}
+    </ul>
+  );
+}
+
+function RouterView() {
+  return (
+    <Switch>
+      {routes.map(route =>
+        <Route key={route.title} path={route.path} exact={route.exact}>
+          <route.component />
+        </Route>
+      )}
+    </Switch>
+  );
+}
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div className="route-loading">loading...</div>}>
+        <RouterView />
+        <RouterLink />
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
