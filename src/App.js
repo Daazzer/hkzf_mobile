78221a42353bae91fd38f210caf6de15b2c626d9
Daazzer <Daazzer@github.com>
2020-12-17 16:47:26 +0800

    将 tabbar 抽离到路由之中，点击导航栏时跳转路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Router from './router'
-import TabBarExample from './components/TabBar';
 
 function App() {
   return (
     <div className="App">
       <Router />
-      <TabBarExample />
     </div>
   );
 }
