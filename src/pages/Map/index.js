d68a385b8b7cbddf7c01390debd289df18790550
Daazzer <Daazzer@github.com>
2020-12-19 21:59:07 +0800

    地图布局

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,16 +1,27 @@
 import { Component } from 'react';
+import { NavBar } from 'antd-mobile';
 import './index.scss';
 
 export class Map extends Component {
   componentDidMount() {
-    const map = new window.BMap.Map('map');
+    const map = new window.BMap.Map('mapGL');
     const point = new window.BMap.Point(116.404, 39.915);
     map.centerAndZoom(point, 15);
   }
 
   render() {
+    const history = this.props.history;
+
     return (
-      <div className="map" id="map"></div>
+      <div className="map">
+        <NavBar
+          style={{ position: 'fixed', width: '100%', zIndex: 100, top: 0 }}
+          mode="light"
+          icon={<i className="iconfont icon-back" style={{ color: '#333' }}></i>}
+          onLeftClick={() => history.goBack()}
+        >地图找房</NavBar>
+        <div className="map-gl" id="mapGL"></div>
+      </div>
     );
   }
 }
