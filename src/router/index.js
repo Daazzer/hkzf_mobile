39f08ef426515959b7f8551cdb59746568dd0963
Daazzer <Daazzer@github.com>
2020-12-19 21:29:27 +0800

    路由结构修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,102 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, Component } from 'react';
 import { TabBar, ActivityIndicator } from 'antd-mobile';
 import {
   BrowserRouter,
   Switch,
-  Route,
-  useHistory,
-  useLocation
+  Route
 } from 'react-router-dom';
 
 const routes = [
   {
     path: '/',
-    title: '首页',
+    name: '首页',
     icon: 'ind',
     component: lazy(() => import('@/pages/Home')),
+    navTab: true,
     exact: true
   },
   {
     path: '/findhouse',
-    title: '找房',
+    name: '找房',
     icon: 'findHouse',
+    navTab: true,
     component: lazy(() => import('@/pages/FindHouse')),
   },
   {
     path: '/news',
-    title: '资讯',
+    name: '资讯',
     icon: 'infom',
+    navTab: true,
     component: lazy(() => import('@/pages/News')),
   },
   {
     path: '/user',
-    title: '我的',
+    name: '我的',
     icon: 'my',
+    navTab: true,
     component: lazy(() => import('@/pages/User'))
+  },
+  {
+    path: '/map',
+    name: '地图',
+    component: lazy(() => import('@/pages/Map'))
   }
 ];
 
-function RouterTabNav() {
-  const pathname = useLocation().pathname;
-  const history = useHistory();
+class RouterTabNav extends Component {
+  render() {
+    const pathname = this.props.location.pathname;
+    const history = this.props.history;
+    const navTabs = routes.filter(route => route.navTab);
 
-  return (
-    <div className="tab-bar-nav">
-      <TabBar
-        unselectedTintColor="#949494"
-        tintColor="#21b97a"
-        barTintColor="white"
-      >
-        {routes.map(route =>
-          <TabBar.Item
-            title={route.title}
-            key={route.title}
-            icon={<i className={`iconfont icon-${route.icon}`}></i>}
-            selectedIcon={<i className={`iconfont icon-${route.icon}`}></i>}
-            selected={pathname === route.path}
-            onPress={() => history.push(route.path)}
-          />
-        )}
-      </TabBar>
-    </div>
-  );
+    return (
+      <div className="tab-bar-nav">
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#21b97a"
+          barTintColor="white"
+        >
+          {navTabs.map(navTab =>
+            <TabBar.Item
+              title={navTab.name}
+              key={navTab.name}
+              icon={<i className={`iconfont icon-${navTab.icon}`}></i>}
+              selectedIcon={<i className={`iconfont icon-${navTab.icon}`}></i>}
+              selected={pathname === navTab.path}
+              onPress={() => history.push(navTab.path)}
+            />
+          )}
+        </TabBar>
+      </div>
+    );
+  }
 }
 
-function RouterView() {
-  return (
-    <Switch>
-      {routes.map(route =>
-        <Route key={route.title} path={route.path} exact={route.exact}>
-          <route.component />
-        </Route>
-      )}
-    </Switch>
-  );
+class RouterView extends Component {
+  render() {
+    return (
+      <Switch>
+        {routes.map(route =>
+          <Route
+            key={route.name}
+            path={route.path}
+            exact={route.exact}
+            children={routeProps => {
+              if (route.navTab) {
+                return (
+                  <>
+                    <route.component {...routeProps} />
+                    <RouterTabNav {...routeProps} />
+                  </>
+                );
+              }
+              return <route.component {...routeProps} />;
+            }
+          } />
+        )}
+      </Switch>
+    );
+  }
 }
 
 function Router() {
@@ -80,7 +105,6 @@ function Router() {
       <Suspense fallback={<ActivityIndicator className="router-loading" size="large" text="加载中..." />}>
         <RouterView />
       </Suspense>
-      <RouterTabNav />
     </BrowserRouter>
   );
 }
