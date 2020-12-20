fd0cc055d55227576bf2dc679b23c06b4ed1fea5
Daazzer <Daazzer@github.com>
2020-12-20 14:59:34 +0800

    引入 react-virtualized

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'react-virtualized/styles.css';
 import './assets/fonts/iconfont.css';
 import './index.css';
 
