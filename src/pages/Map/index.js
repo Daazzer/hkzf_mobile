fc6d596f07e2c16b8b879f2f9a6c8750d5df0304
Daazzer <Daazzer@github.com>
2020-12-20 09:40:52 +0800

    地图样式调整，标记生成

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -6,7 +6,31 @@ export class Map extends Component {
   componentDidMount() {
     const map = new window.BMap.Map('mapGL');
     const point = new window.BMap.Point(116.404, 39.915);
+    const point1 = new window.BMap.Point(116.397, 39.715);
     map.centerAndZoom(point, 15);
+    map.addControl(new window.BMap.NavigationControl());
+    const opts = {
+      position: point,
+      offset: new window.BMap.Size(-35, -35)
+    };
+    const label = new window.BMap.Label(
+      `<div class="map-bubble">
+        <p class="map-bubble__title">番禺</p>
+        <p class="map-bubble__content">120套</p>
+      </div>`,
+      opts
+    );
+    label.setStyle({
+      color: '#fff',
+      borderRadius: '50%',
+      border: '2px solid #fff',
+      fontSize: '12px',
+      height: '70px',
+      width: '70px',
+      backgroundColor: 'rgba(12, 181, 106, 0.8)'
+    });
+    map.addOverlay(label);
+    map.setViewport([point]);
   }
 
   render() {
@@ -14,13 +38,13 @@ export class Map extends Component {
 
     return (
       <div className="map">
+        <div className="map-gl" id="mapGL"></div>
         <NavBar
-          style={{ position: 'fixed', width: '100%', zIndex: 100, top: 0 }}
+          style={{ position: 'fixed', width: '100%', top: 0 }}
           mode="light"
           icon={<i className="iconfont icon-back" style={{ color: '#333' }}></i>}
           onLeftClick={() => history.goBack()}
         >地图找房</NavBar>
-        <div className="map-gl" id="mapGL"></div>
       </div>
     );
   }
