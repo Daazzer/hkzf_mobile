7a674893de4a43c2ddc75a6f306d5465931f488a
Daazzer <Daazzer@github.com>
2020-12-21 15:10:04 +0800

    新增详情页

---
+++ src/pages/Detail/index.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+export class Detail extends Component {
+  render() {
+    return (
+      <div>
+        详情页
+      </div>
+    );
+  }
+}
+
+export default Detail;
