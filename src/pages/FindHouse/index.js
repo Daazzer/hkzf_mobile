9f4683ef5e0f4d5f4f1378e6d6e12cc3e02e2322
Daazzer <Daazzer@github.com>
2020-12-21 16:52:08 +0800

    找房页 SearchBar

--- src/pages/FindHouse/index.js
+++ src/pages/FindHouse/index.js
@@ -1,10 +1,14 @@
 import { Component } from 'react';
+import { NavBar } from 'antd-mobile';
+import SearchBar from '../../components/SearchBar';
 
 export class FindHouse extends Component {
   render() {
     return (
-      <div>
-        找房
+      <div className="findhouse">
+        <NavBar className="nav-header" mode="light" leftContent={<i className="iconfont icon-back"></i>}>
+          <SearchBar mapIconColor="#00ae66" />
+        </NavBar>
       </div>
     );
   }
