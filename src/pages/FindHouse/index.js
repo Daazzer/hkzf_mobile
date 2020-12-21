b3dfcbf834e308bce70f0361649103b816b92d1e
Daazzer <Daazzer@github.com>
2020-12-21 21:37:44 +0800

    找房页房源信息列表渲染及布局

--- src/pages/FindHouse/index.js
+++ src/pages/FindHouse/index.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { NavBar, Toast } from 'antd-mobile';
 import SearchBar from '../../components/SearchBar';
 import Filter from '../../components/FindHouse/Filter';
+import HouseInfoList from '../../components/HouseInfoList';
 import api from '../../utils/api';
 import map from '../../utils/map';
 import storage from '../../utils/storage';
@@ -14,7 +15,9 @@ export class FindHouse extends Component {
       city: {
         label: '',
         value: ''
-      }
+      },
+      start: 1,
+      houseInfoItems: []
     };
   }
 
@@ -33,6 +36,34 @@ export class FindHouse extends Component {
     return city;
   }
 
+  async renderHouseInfoItems(cityId) {
+    Toast.loading('获取房源信息中...');
+    const [err, res] = await api.getHouses({
+      cityId,
+      start: 1,
+      end: 20
+    });
+
+    if (err) {
+      Toast.hide();
+      Toast.fail('获取房源信息失败');
+      return;
+    }
+
+    Toast.hide();
+    const houseInfoItems = res.data.body.list.map(houseInfoItem => {
+      let houseImg = houseInfoItem.houseImg;
+      if (!(/^https?:\/\//.test(houseImg))) {
+        houseImg = res.config.baseURL + houseInfoItem.houseImg
+      }
+      return {
+        ...houseInfoItem,
+        houseImg
+      }
+    });
+    this.setState({ houseInfoItems })
+  }
+
   async componentDidMount() {
     let city = storage.getData('city');
     if (!city) {
@@ -40,6 +71,7 @@ export class FindHouse extends Component {
       storage.setData('city', city);
     }
     this.setState({ city });
+    this.renderHouseInfoItems(city.value);
   }
 
   render() {
@@ -54,6 +86,7 @@ export class FindHouse extends Component {
           <SearchBar mapIconColor="#00ae66" cityName={this.state.city.label} />
         </NavBar>
         <Filter />
+        <HouseInfoList houseInfoItems={this.state.houseInfoItems} />
       </div>
     );
   }
