36262e787d9022180c435df36aeaeb3a532ad5c0
Daazzer <Daazzer@github.com>
2020-12-22 14:45:09 +0800

    FilterTitle 组件布局

--- src/components/FindHouse/FilterTitle/index.js
+++ src/components/FindHouse/FilterTitle/index.js
@@ -2,18 +2,29 @@ import { Flex } from 'antd-mobile';
 import './index.scss';
 
 function FilterTitle() {
+  const curIndex = 0;
   const filterItems = [
-    '区域',
-    '方式',
-    '租金',
-    '筛选'
+    {
+      title: '区域',
+    },
+    {
+      title: '方式',
+    },
+    {
+      title: '租金',
+    },
+    {
+      title: '筛选',
+    }
   ];
+
+  const activeClass = i => i === curIndex ? 'active' : '';
 
   return (
     <Flex className="filter-title" align="center">
-      {filterItems.map(filterItem =>
-        <Flex.Item className="filter-title__item" key={filterItem}>
-          {filterItem}
+      {filterItems.map((filterItem, index) =>
+        <Flex.Item className={`filter-title__item ${activeClass(index)}`} key={filterItem.title}>
+          {filterItem.title}
           <i className="iconfont icon-arrow"></i>
         </Flex.Item>
       )}
