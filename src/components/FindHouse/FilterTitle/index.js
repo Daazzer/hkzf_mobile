897ea903c735b7c38be720d359a883c79c3cf957
Daazzer <Daazzer@github.com>
2020-12-22 15:13:27 +0800

    改造 FilterTitle 组件

--- src/components/FindHouse/FilterTitle/index.js
+++ src/components/FindHouse/FilterTitle/index.js
@@ -1,35 +1,57 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Flex } from 'antd-mobile';
 import './index.scss';
 
-function FilterTitle() {
-  const curIndex = 0;
-  const filterItems = [
-    {
-      title: '区域',
-    },
-    {
-      title: '方式',
-    },
-    {
-      title: '租金',
-    },
-    {
-      title: '筛选',
-    }
-  ];
+class FilterTitle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterTitleItems: [
+        {
+          title: '区域',
+          type: 'area'
+        },
+        {
+          title: '方式',
+          type: 'mode'
+        },
+        {
+          title: '租金',
+          type: 'price'
+        },
+        {
+          title: '筛选',
+          type: 'more'
+        }
+      ]
+    };
+  }
 
-  const activeClass = i => i === curIndex ? 'active' : '';
-
-  return (
-    <Flex className="filter-title" align="center">
-      {filterItems.map((filterItem, index) =>
-        <Flex.Item className={`filter-title__item ${activeClass(index)}`} key={filterItem.title}>
-          {filterItem.title}
-          <i className="iconfont icon-arrow"></i>
-        </Flex.Item>
-      )}
-    </Flex>
-  );
+  render() {
+    const { activeType, onOpen } = this.props;
+    const filterTitleItems = this.state.filterTitleItems;
+    const activeClass = type => activeType === type ? 'active' : '';
+    return (
+      <Flex className="filter-title" align="center">
+        {filterTitleItems.map(filterTitleItem =>
+          <Flex.Item
+            className={`filter-title__item ${activeClass(filterTitleItem.type)}`}
+            key={filterTitleItem.title}
+            onClick={() => onOpen(filterTitleItem.type)}
+          >
+            {filterTitleItem.title}
+            <i className="iconfont icon-arrow"></i>
+          </Flex.Item>
+        )}
+      </Flex>
+    );
+  }
 }
+
+FilterTitle.propTypes = {
+  activeType: PropTypes.string,
+  onOpen: PropTypes.func
+};
 
 export default FilterTitle;
