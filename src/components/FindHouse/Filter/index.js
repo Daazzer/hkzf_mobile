bde3612a3c39a39e14fc7daf553d1476d9293528
Daazzer <Daazzer@github.com>
2020-12-22 15:42:36 +0800

    添加组件渲染条件

--- src/components/FindHouse/Filter/index.js
+++ src/components/FindHouse/Filter/index.js
@@ -1,19 +1,47 @@
+import { Component } from 'react';
 import FilterTitle from '../FilterTitle';
 import FilterPicker from '../FilterPicker';
 import FilterFooter from '../FilterFooter';
 import './index.scss';
 
-function Filter() {
-  return (
-    <div className="filter">
-      <div className="filter-mask"></div>
-      <div className="filter-content">
-        <FilterTitle />
-        <FilterPicker />
-        <FilterFooter />
+class Filter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeType: ''
+    };
+    this.onOpen = this.onOpen.bind(this);
+    this.onClose = this.onClose.bind(this);
+  }
+
+  onOpen(activeType) {
+    this.setState({ activeType });
+    document.body.classList.add('body-fixed');
+  }
+
+  onClose() {
+    this.setState({ activeType: '' });
+    document.body.classList.remove('body-fixed');
+  }
+
+  render() {
+    const activeType = this.state.activeType;
+    const isActiveComponent = ['area', 'mode', 'price'].indexOf(activeType) !== -1;
+
+    return (
+      <div className="filter">
+        {isActiveComponent ? <div className="filter-mask" onClick={this.onClose}></div> : null}
+        <div className="filter-content">
+          <FilterTitle
+            onOpen={this.onOpen}
+            activeType={activeType}
+          />
+          {isActiveComponent ? <FilterPicker /> : null}
+          {isActiveComponent ? <FilterFooter /> : null}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Filter;
