ba6e58cecc6d6868d56a073fa1a75af092bac67a
Daazzer <Daazzer@github.com>
2020-12-22 14:46:15 +0800

    Filter 组件布局

--- src/components/FindHouse/Filter/index.js
+++ src/components/FindHouse/Filter/index.js
@@ -1,9 +1,17 @@
 import FilterTitle from '../FilterTitle';
+import FilterPicker from '../FilterPicker';
+import FilterFooter from '../FilterFooter';
+import './index.scss';
 
 function Filter() {
   return (
     <div className="filter">
-      <FilterTitle />
+      <div className="filter-mask"></div>
+      <div className="filter-content">
+        <FilterTitle />
+        <FilterPicker />
+        <FilterFooter />
+      </div>
     </div>
   );
 }
