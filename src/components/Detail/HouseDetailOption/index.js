87b0b9df83c82c3459671209ccfe8c11f8e5fbda
Daazzer <Daazzer@github.com>
2020-12-24 17:02:31 +0800

    房屋操作项基本功能实现

--- src/components/Detail/HouseDetailOption/index.js
+++ src/components/Detail/HouseDetailOption/index.js
@@ -1,14 +1,32 @@
 import { Component } from 'react';
-import { Flex } from 'antd-mobile';
+import { withRouter } from 'react-router-dom';
+import { Flex, Modal } from 'antd-mobile';
 import './index.scss';
+const baseURL = process.env.REACT_APP_URL;
 
 export class HouseDetailOption extends Component {
+  constructor() {
+    super();
+    this.handleFavorite = this.handleFavorite.bind(this);
+  }
+
+  handleFavorite() {
+    const { history, location } = this.props;
+    Modal.alert('提示', '登录后才能收藏房源，是否去登录?', [
+      { text: '取消' },
+      {
+        text: '去登录',
+        onPress: () => history.replace('/login', { from: location })
+      }
+    ]);
+  }
+
   render() {
     return (
       <Flex className="house-detail-option">
-        <Flex.Item className="house-detail-option__item">
-          <i className="iconfont icon-coll"></i>
-          收藏
+        <Flex.Item className="house-detail-option__item" onClick={this.handleFavorite}>
+          <img src={baseURL + '/img/unstar.png'} alt="收藏" />
+          <span>收藏</span>
         </Flex.Item>
         <Flex.Item className="house-detail-option__item">
           在线质询
@@ -21,4 +39,6 @@ export class HouseDetailOption extends Component {
   }
 }
 
-export default HouseDetailOption;
+const HouseDetailOptionWithRouter = withRouter(HouseDetailOption);
+
+export default HouseDetailOptionWithRouter;
