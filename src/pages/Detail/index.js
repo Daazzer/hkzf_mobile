9f935c77bd3d870453d7eb7bb20fe2b8ad36b2df
Daazzer <Daazzer@github.com>
2020-12-24 15:40:31 +0800

    房屋详情数据获取

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,25 +1,47 @@
 import { Component } from 'react';
-import { NavBar, Carousel } from 'antd-mobile';
+import { NavBar, Carousel, Toast } from 'antd-mobile';
 import HouseInfo from '../../components/Detail/HouseInfo';
 import HouseMap from '../../components/Detail/HouseMap';
 import HouseAbout from '../../components/Detail/HouseAbout';
 import HouseProfile from '../../components/Detail/HouseProfile';
 import HouseRecommend from '../../components/Detail/HouseRecommend';
 import HouseDetailOption from '../../components/Detail/HouseDetailOption';
+import api from '../../utils/api';
 import './index.scss';
 
 export class Detail extends Component {
   constructor() {
     super();
     this.state = {
-      data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      imgHeight: 176,
+      houseInfo: {
+        houseImg: []
+      }
     };
   }
+
+  async renderHouseInfo(id) {
+    const [err, res] = await api.getHousesById(id);
+
+    if (err) {
+      Toast.fail('获取租房详情信息失败');
+      return;
+    }
+
+    const baseURL = res.config.baseURL;
+    let houseInfo = res.data.body;
+    houseInfo = {
+      ...houseInfo,
+      houseImg: houseInfo.houseImg.map(img => baseURL + img)
+    };
+    this.setState({ houseInfo });
+  }
+
   componentDidMount() {
-    console.log('id: ' + this.props.match.params.id);
+    const id = this.props.match.params.id;
+    this.renderHouseInfo(id);
   }
   render() {
+    const { community, houseImg } = this.state.houseInfo;
     return (
       <div className="house-detail">
         <NavBar
@@ -28,25 +50,25 @@ export class Detail extends Component {
           leftContent={<i className="iconfont icon-back"></i>}
           rightContent={<i className="iconfont icon-share"></i>}
           onLeftClick={() => this.props.history.goBack()}
-        >绿谷康都</NavBar>
+        >{community}</NavBar>
         <Carousel
-          autoplay={false}
+          autoplay
           infinite
           beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
           afterChange={index => console.log('slide to', index)}
+          style={{ height: 252 }}
         >
-          {this.state.data.map(val => (
+          {houseImg.map(img => (
             <a
-              key={val}
+              key={img}
               href="https://lianjia.com"
-              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+              style={{ display: 'inline-block', width: '100%', height: '252px' }}
             >
               <img
-                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                alt=""
-                style={{ width: '100%', verticalAlign: 'top' }}
+                src={img}
+                alt="房屋照片"
+                style={{ display: 'inline-block', width: '100%', height: '100%' }}
                 onLoad={() => {
-                  // fire window resize event to change height
                   window.dispatchEvent(new Event('resize'));
                   this.setState({ imgHeight: 'auto' });
                 }}
