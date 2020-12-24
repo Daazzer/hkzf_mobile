6c84a25ae1be52f852ea8a39a134bed106cf2e08
Daazzer <Daazzer@github.com>
2020-12-24 18:07:47 +0800

    解决样式命名冲突

--- src/components/Detail/HouseInfo/index.js
+++ src/components/Detail/HouseInfo/index.js
@@ -7,33 +7,33 @@ export class HouseInfo extends Component {
     const { title, tags, price, roomType, size, oriented, floor } = this.props;
 
     return (
-      <div className="house-info">
-        <h3 className="house-info__title">{title}</h3>
-        <Flex className="house-info-tags" wrap="wrap">
+      <div className="house-detail-info">
+        <h3 className="house-detail-info__title">{title}</h3>
+        <Flex className="house-detail-info-tags" wrap="wrap">
           {tags.map((tag, i) =>
-            <span key={tag} className={`house-info-tags__item house-info-tags__item--${(i%6)+1}`}>{tag}</span>
+            <span key={tag} className={`house-detail-info-tags__item house-detail-info-tags__item--${(i%6)+1}`}>{tag}</span>
           )}
         </Flex>
-        <Flex className="house-info-type-info">
-          <Flex.Item className="house-info-type-info__item house-info-type-info__item--price">
+        <Flex className="house-detail-info-type-info">
+          <Flex.Item className="house-detail-info-type-info__item house-detail-info-type-info__item--price">
             <h4>{price}<span>/月</span></h4>
             <p>租金</p>
           </Flex.Item>
-          <Flex.Item className="house-info-type-info__item house-info-type-info__item--room-type">
+          <Flex.Item className="house-detail-info-type-info__item house-detail-info-type-info__item--room-type">
             <h4>{roomType}</h4>
             <p>房型</p>
           </Flex.Item>
-          <Flex.Item className="house-info-type-info__item house-info-type-info__item--size">
+          <Flex.Item className="house-detail-info-type-info__item house-detail-info-type-info__item--size">
             <h4>{size}平米</h4>
             <p>面积</p>
           </Flex.Item>
         </Flex>
-        <Flex className="house-info-base-info">
-          <Flex.Item className="house-info-base-info__item">
+        <Flex className="house-detail-info-base-info">
+          <Flex.Item className="house-detail-info-base-info__item">
             <p><span>装修：</span>精装</p>
             <p><span>楼层：</span>{floor}</p>
           </Flex.Item>
-          <Flex.Item className="house-info-base-info__item">
+          <Flex.Item className="house-detail-info-base-info__item">
             <p><span>朝向：</span>{oriented.join('、')}</p>
             <p><span>类型：</span>普通住宅</p>
           </Flex.Item>
