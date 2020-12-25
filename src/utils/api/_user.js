9f89f7224d6cc89a9685f637e2b12a46aaee522d
Daazzer <Daazzer@github.com>
2020-12-25 09:13:07 +0800

    修改 api 注释

--- src/utils/api/_user.js
+++ src/utils/api/_user.js
@@ -2,10 +2,9 @@ import http from '../http';
 
 /**
  * 用户登录接口
- * @param {Object} data 参数对象
- * @param {Object} data.body 用户登录的时候需要提交的内容
- * @param {string} data.body.username 用户名
- * @param {string} data.body.password 密码
+ * @param {Object} data 用户登录的时候需要提交的内容
+ * @param {string} data.username 用户名
+ * @param {string} data.password 密码
  */
 const login = data => http.post('/user/login', data);
 
