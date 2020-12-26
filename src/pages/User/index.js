514081b3c3355884b5829888ef0a6417e3abeb20
Daazzer <Daazzer@github.com>
2020-12-26 10:30:11 +0800

    新增页面跳转链接

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -8,13 +8,14 @@ import './index.scss';
 
 // 菜单数据
 const menus = [
-  { id: 1, name: '我的收藏', iconfont: 'icon-coll', to: '/favorate' },
+  { id: 1, name: '我的收藏', iconfont: 'icon-coll', to: '/favorite' },
   { id: 2, name: '我的出租', iconfont: 'icon-ind', to: '/rent' },
   { id: 3, name: '看房记录', iconfont: 'icon-record' },
   {
     id: 4,
     name: '成为房主',
-    iconfont: 'icon-identity'
+    iconfont: 'icon-identity',
+    to: '/rent/add'
   },
   { id: 5, name: '个人资料', iconfont: 'icon-myinfo' },
   { id: 6, name: '联系我们', iconfont: 'icon-cust' }
